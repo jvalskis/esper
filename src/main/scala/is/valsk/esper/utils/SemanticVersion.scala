21d2fd26a572455573be41ae0b263e04c7b767ce
Justas Valskis <4158759+jvalskis@users.noreply.github.com>
2023-04-04 18:38:42 +0300

    Adding semantic version abstraction

---
+++ src/main/scala/is/valsk/esper/utils/SemanticVersion.scala
@@ -0,0 +1,46 @@
+package is.valsk.esper.utils
+
+import scala.annotation.tailrec
+
+
+case class SemanticVersion(
+    version: String,
+) extends Ordered[SemanticVersion] {
+  override def compare(that: SemanticVersion): Int = {
+    compareSemantically(version.split("\\.").toList, that.version.split("\\.").toList)
+  }
+
+  @tailrec
+  private def compareSemantically(thisVersion: List[String], otherVersion: List[String]): Int = {
+    (thisVersion, otherVersion) match {
+      case (Nil, Nil) => 0
+      case (Nil, _) => -1
+      case (_, Nil) => 1
+      case (SemanticVersionSegment(thisNumber, thisSuffix) :: thisTail, SemanticVersionSegment(otherNumber, otherSuffix) :: otherTail) =>
+        if (thisNumber > otherNumber) 1
+        else if (thisNumber < otherNumber) -1
+        else if (thisSuffix > otherSuffix) 1
+        else if (thisSuffix < otherSuffix) -1
+        else compareSemantically(thisTail, otherTail)
+      case (thisString :: thisTail, otherString :: otherTail) =>
+        if (thisString > otherString) 1
+        else if (thisString < otherString) -1
+        else compareSemantically(thisTail, otherTail)
+    }
+  }
+}
+
+case class SemanticVersionSegment(
+    number: Int,
+    suffix: String
+)
+
+object SemanticVersionSegment {
+
+  def unapply(string: String): Option[SemanticVersionSegment] = {
+    val prefix = string.takeWhile(x => !x.isDigit)
+    val number = string.drop(prefix.length).takeWhile(_.isDigit)
+    val suffix = string.drop(prefix.length + number.length)
+    Some(SemanticVersionSegment(number.toIntOption.getOrElse(0), suffix))
+  }
+}
---
+++ src/test/scala/is/valsk/esper/SemanticVersionSuite.scala
@@ -0,0 +1,41 @@
+package is.valsk.esper
+
+import io.circe
+import io.circe.Decoder
+import io.circe.*
+import io.circe.generic.semiauto.*
+import io.circe.generic.semiauto.deriveDecoder
+import is.valsk.esper.hass.messages.Type.*
+import is.valsk.esper.hass.messages.responses.AuthRequired.decoder
+import zio.json.*
+import io.circe.syntax.*
+import is.valsk.esper.hass.messages.commands.{Auth, DeviceRegistryList}
+import is.valsk.esper.hass.messages.responses.{AuthRequired, Result}
+import is.valsk.esper.utils.SemanticVersion
+import munit.Suite
+import zio.ZIO
+import zio.test.*
+import zio.test.Assertion.*
+
+object SemanticVersionSuite extends ZIOSpecDefault {
+
+  def spec = suite("Semantic version comparison")(ZIO.succeed(
+    List(
+      ("1.0.0", "1.0.0", 0),
+      ("1.0.0", "1.0.1", -1),
+      ("1.0.1", "1.0.0", 1),
+      ("1.0.1", "1.0.10", -1),
+      ("1.0.2", "1.0.10", -1),
+      ("1.0.10", "1.0.1", 1),
+      ("1.0.1-suffix", "1.0.1", 1),
+      ("1.0.1-suffix", "1.0.2", -1),
+      ("1.0.1-suffix", "1.0.1-suffix1", -1),
+    ).map((testCase _).tupled)
+  ))
+
+  private def testCase(string1: String, string2: String, expected: Int): Spec[Any, Nothing] = {
+    test(s"$string1 compareTo $string2 => $expected") {
+      assertTrue(SemanticVersion(string1).compareTo(SemanticVersion(string2)) == expected)
+    }
+  }
+}
