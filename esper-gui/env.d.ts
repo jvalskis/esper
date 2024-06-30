f3d3733a0bf9f471be88f1dbf97b1d6c037cd9c1
Justas Valskis <4158759+jvalskis@users.noreply.github.com>
2024-07-01 01:55:44 +0300

    Remove unused configuration

--- esper-gui/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly VITE_ESPER_API_ADDRESS: string
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv
-}
--- esper-gui/src/config.ts
+++
@@ -1,7 +0,0 @@
-export interface Config {
-    ESPER_API_ADDRESS: string
-}
-
-export const config: Config = {
-    ESPER_API_ADDRESS: import.meta.env.VITE_ESPER_API_ADDRESS,
-}
